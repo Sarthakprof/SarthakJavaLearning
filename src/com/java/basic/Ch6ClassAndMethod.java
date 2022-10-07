9d501fa9ed3e3b024ca24e32eadb539ea5e27bb7
Sarthak Srivastava <38829053+Sarthakprof@users.noreply.github.com>
2022-10-07 21:32:57 +0530

    Updated

--- src/com/java/basic/Ch6ClassAndMethod.java
+++ src/com/java/basic/Ch6ClassAndMethod.java
@@ -2,18 +2,29 @@ package com.java.basic;
 
 //Class is like an template
 public class Ch6ClassAndMethod {
+	int var1 = 5;
 	
 	//method
-	public void revolve() {
+	public static void revolve() {
 		System.out.println("Revolve");
+	}
+	
+	public void revolve2() {
+		System.out.println("Revolve2");
 	}
 
 	public static void main(String[] args) {
 		
-		//object call the method of class
-		Ch6ClassAndMethod obj = new Ch6ClassAndMethod();
-		obj.revolve();
+		//call the method of class 
 
+		revolve();
+
+		//calling non-static method by creating an instance of class
+		Ch6ClassAndMethod obj = new Ch6ClassAndMethod();
+		obj.revolve2();
+		
+		//accessing variables from ojbects
+		System.out.println(obj.var1);
 	}
 
 }
